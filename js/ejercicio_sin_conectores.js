be73c33aa922064e012a8c67c84f556f3554942f
priscilabgmoya <105519676+priscilabgmoya@users.noreply.github.com>
2023-04-13 11:36:12 -0300

    Update ejercicio_sin_conectores.js

--- js/ejercicio_sin_conectores.js
+++ js/ejercicio_sin_conectores.js
@@ -5,8 +5,6 @@
    - si la vida esta entre 51 y 80 que el personaje se ponga en modo moderado.
    - si la vida esta entre 81 y 100 que el personaje se ponga en modo ofensivo.
 
- b) resolver con conectores lógicos (contemplar que tantos valores a cero y mayores a cien no pueden ser validos).
-
   a) resolver sin conectores lógicos
 
  */
@@ -15,19 +13,6 @@ const acciones_personaje = ["HUYA", "MODO DEFENSIVO", "MODO MODERADO", "MODO OFE
 
 let vida_personaje = parseInt(prompt('Ingrese vida del personaje'));
 
-
-if(! isNaN(vida_personaje) && vida_personaje >0 && vida_personaje <=100 ){
-    if (vida_personaje <= 20){
-        alert(`ACCION: ${acciones_personaje[0]}`);
-    }else if(vida_personaje >= 21 && vida_personaje <= 50 ){
-        alert(`ACCION: ${acciones_personaje[1]}`);
-    }else if(vida_personaje >= 51 && vida_personaje <=80){
-        alert(`ACCION: ${acciones_personaje[2]}`);
-    }else if (vida_personaje >=81){
-        alert(`ACCION: ${acciones_personaje[3]}`);
-    }
-}else{
-    alert('ingrese una vida correcta. Vidas mayores a 0 y menores a 100');
 
 if(! isNaN(vida_personaje)){
 if(vida_personaje >0 ){
